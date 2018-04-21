a9241ae973f398a080b10cb44463abad502ba8ba
porrametict <37680321+porrametict@users.noreply.github.com>
2018-04-21 11:18:43 +0700

    Bigupdate

--- test/getResult.py
+++ test/getResult.py
@@ -1,7 +1,10 @@
 import numpy as np
-def getResult(frist,second,operator) :
-    if operator == "!" : #frist = True False second = None 
-        return not(frist)
+def getResult(frist:bool,second:bool,operator:str) :
+    if operator == "!" : #frist = True False second = None
+        if type(second) == type(None):
+            return not(frist)
+        else:
+            return not(second)
     elif operator == "&":
         return np.logical_and(frist,second)
     elif operator == "|" :
@@ -10,5 +13,6 @@ def getResult(frist,second,operator) :
         return np.logical_or(not(frist),second)
     elif operator == "%" :
         return not(np.logical_xor(frist,second))
-print(getResult(True,False,"!"))
+#print(getResult(True,False,"!"))
+
  
